6352fd650c467e10c3f069529f12ed626b45649d
boris-valero <157314735+boris-valero@users.noreply.github.com>
2025-04-29 23:17:57 +0200

    Create PageController.php

---
+++ lib/Controller/PageController.php
@@ -0,0 +1,21 @@
+<?php
+namespace OCA\MyApp\Controller;
+
+use OCP\AppFramework\Controller;
+use OCP\AppFramework\Http\TemplateResponse;
+use OCP\IRequest;
+
+class PageController extends Controller {
+
+    public function __construct($appName, IRequest $request) {
+        parent::__construct($appName, $request);
+    }
+
+    /**
+     * @NoAdminRequired
+     * @NoCSRFRequired
+     */
+    public function index() {
+        return new TemplateResponse($this->appName, 'index');
+    }
+}
